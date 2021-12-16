942f15b83a2fc4602555d9595851acdeed4aa4a8
Wentao Wu <david830wu@gmail.com>
2021-12-16 16:18:27 +0800

    Fixed sed for MacOS

--- scripts/render.sh
+++ scripts/render.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
 # set project init variables
-ProjectName="ProjectName"
+ProjectName="MyProjectName"
 Description="Description of project"
 Author="Author"
 Today=$(date '+%Y%m%d')
 Version="0.1"
+Machine="$(uname -s)"
 
-# render template files
+ReplaceItems="\
+s,{{ProjectName}},${ProjectName},g;\
+s,{{Description}},${Description},g;\
+s,{{Author}},${Author},g;\
+s,{{Today}},${Today},g;\
+s,{{Version}},${Version},g;\
+" 
+
 echo "Rendering file templates"
-find . -type f -exec \
-    sed -i "s,{{ProjectName}},${ProjectName},g; \
-            s,{{Description}},${Description},g; \
-            s,{{Author}},${Author},g; \
-            s,{{Today}},${Today},g; \
-            s,{{Version}},${Version},g; \
-    " {} \;
+case "${Machine}" in
+    Linux*)  
+        find . -type d -name .git -prune -o -type f -print0 | xargs -0 sed -i "${ReplaceItems}"
+        ;;
+    Darwin*) 
+        find . -type d -name .git -prune -o -type f -print0 | LC_ALL=C xargs -0 sed -i "" "${ReplaceItems}"
+        ;;
+    *)
+esac
 
 # rename files
 echo "Rendering file names"
@@ -25,3 +35,4 @@ mv "tests/ProjectNameTest.cpp" "tests/${ProjectName}Test.cpp"
 mv "cmake/ProjectNameConfig.cmake" "cmake/${ProjectName}Config.cmake"
 
 echo "All Done"
+
